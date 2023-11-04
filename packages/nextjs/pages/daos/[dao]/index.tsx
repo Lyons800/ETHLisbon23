3f19c36d136a6f8969af7d54509557d2e0691d5c
Lyons800 <oisinlyons13@gmail.com>
2023-11-04 18:17:00 +0000

    add stuff

--- packages/nextjs/pages/daos/[dao]/index.tsx
+++ packages/nextjs/pages/daos/[dao]/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 import ProposalsList from "~~/components/ProposalsList";
 
@@ -12,7 +13,7 @@ function ProposalsPage() {
     { id: 2, title: "Proposal 2", description: "This is the second proposal", daoId: 2 },
     { id: 3, title: "Proposal 3", description: "This is the third proposal", daoId: 3 },
   ];
-  // fetch proposals (nfts) based on the DAO id from the smart contract
+  // fetch proposals (smart contracts) based on the DAO id from the smart contract
 
   //   useEffect(() => {
   //     async function fetchProposals() {
@@ -30,6 +31,11 @@ function ProposalsPage() {
     <div className="flex flex-col gap-y-6 lg:gap-y-8 py-8 lg:py-12 lg-mx-12 justify-center items-center">
       <h1>Proposals for DAO {dao}</h1>
       {/* Access the proposals array */}
+
+      {/*add a link button to go back to the /dao page */}
+      <div className="flex border rounded-full border-y-cyan-700 py-2 px-4">
+        <Link href="/daos">Back to DAOs</Link>
+      </div>
       <ProposalsList proposals={proposals} />
     </div>
   );
