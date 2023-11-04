770407d8722e5a44fdebe46bd9a54f9f60d3fde0
Lyons800 <oisinlyons13@gmail.com>
2023-11-04 19:19:53 +0000

    add nft call

--- packages/nextjs/pages/daos/[dao]/[proposal]/index.tsx
+++ packages/nextjs/pages/daos/[dao]/[proposal]/index.tsx
@@ -1,8 +1,11 @@
 import { useRouter } from "next/router";
 import { NextPage } from "next";
+import { getContractNFTs, getNFTMetadata } from "~~/services/web3/moralis";
 
 interface ProposalPageProps {
   proposals: Array<{ id: string; title: string; description: string }>;
+  nfts: any; // Add type definition for the NFTs data structure
+  metadata: any; // Add type definition for the metadata data structure
 }
 
 const ProposalPage: NextPage<ProposalPageProps> = ({ proposals }) => {
@@ -13,8 +16,6 @@ const ProposalPage: NextPage<ProposalPageProps> = ({ proposals }) => {
   // Ensure proposals is defined before trying to find the selected proposal
   const selectedProposal = proposals?.find(p => p.id === proposal);
 
-  //fetch proposal smart contract from the dao smart contract and fetch all related nfts in this smart contract
-
   // Render the proposal page
   return (
     <div>
@@ -22,6 +23,7 @@ const ProposalPage: NextPage<ProposalPageProps> = ({ proposals }) => {
         <>
           <h1>{selectedProposal.title}</h1>
           <p>{selectedProposal.description}</p>
+
           {/* Render other proposal content here */}
         </>
       ) : (
@@ -30,5 +32,24 @@ const ProposalPage: NextPage<ProposalPageProps> = ({ proposals }) => {
     </div>
   );
 };
+
+// Fetch data on server-side
+export async function getServerSideProps() {
+  const contract = "0x22C1f6050E56d2876009903609a2cC3fEf83B415";
+  const chain = "gnosis";
+
+  // Fetch data from Moralis API
+  const nfts = await getContractNFTs(chain, contract);
+  const metadata = await getNFTMetadata(chain, contract, "1"); // You might want to change the tokenId based on your use case
+
+  // Return as props
+  return {
+    props: {
+      nfts,
+      metadata,
+      proposals: [], // Add your logic to fetch proposals or pass them as static data
+    },
+  };
+}
 
 export default ProposalPage;
